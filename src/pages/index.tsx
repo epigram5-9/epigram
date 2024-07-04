ddd7068fd89ec980d4b68a3020e5f7e1093794b2
morrison <morrison.k@hpcnt.com>
2024-07-04 20:00:58 +0900

    afkbasdf

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
 import { useEffect, useState } from 'react';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   // NOTE: 테스트를 위해서 typescript-eslint/no-unused-vars도 잠시 끔!
@@ -32,7 +29,7 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <main className={`${styles.main}`}>
         <div className={styles.description}>
           <p className='font-bold underline'>
             Get started by editing&nbsp;
