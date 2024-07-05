121bb102abe964cb42474fd210b69372d6e5c074
전유민 <beforeyum@jeon-yumin-ui-noteubug.local>
2024-07-05 17:26:05 +0900

    fix: README.md & userQueryHooks.ts 수정

--- src/hooks/userQueryHooks.ts
+++ src/hooks/userQueryHooks.ts
@@ -1,13 +1,23 @@
 import quries from '@/apis/queries';
 import { updateMe } from '@/apis/user';
-import { GetUserRequestType, PatchMeRequestType } from '@/schema/user';
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { GetUserReponseType, GetUserRequestType, PatchMeRequestType } from '@/schema/user';
+import { MutationOptions } from '@/types/query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 export const useMeQuery = () => useQuery(quries.user.getMe());
 
 export const useUserQuery = (requset: GetUserRequestType) => useQuery(quries.user.getUser(requset));
 
-export const useUpdateMe = () =>
-  useMutation({
+export const useUpdateMe = (options: MutationOptions<GetUserReponseType>) => {
+  const queryClient = useQueryClient();
+  return useMutation({
     mutationFn: (request: PatchMeRequestType) => updateMe(request),
+    ...options,
+    onSuccess: (...arg) => {
+      queryClient.invalidateQueries(quries.user.getMe());
+      if (options?.onSuccess) {
+        options?.onSuccess(...arg);
+      }
+    },
   });
+};
