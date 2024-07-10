4f493bded5fafba8d2d5c013c6a80136f4d1e4fd
임수혁 <skilldoy2@naver.com>
2024-07-10 13:18:17 +0900

    feat: RecentSearches UI 초안 완성

--- src/components/search/RecentSearches.tsx
+++ src/components/search/RecentSearches.tsx
@@ -3,9 +3,9 @@ function RecentSearches() {
     <div>
       <div className='flex justify-between items-center'>
         <span className='text-black-700 font-pretendard text-2xl font-medium'>최근 검색어</span>
-        <span>모두 지우기</span>
+        <span className='text-state-error font-pretendard text-custom-lg font-semibold'>모두 지우기</span>
       </div>
-      <span>#나아가야할때</span>
+      <span className='flex flex-col justify-center items-center p-[12px] px-[14px] gap-[8px] rounded-[22px] bg-background-100'>#나아가야할때</span>
     </div>
   );
 }
