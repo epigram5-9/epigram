1f8218d57cfdfd8408a5a79183638903415fc58b
임수혁 <skilldoy2@naver.com>
2024-07-10 15:11:54 +0900

    �styles: RecentSearches UI 수정

--- src/components/search/RecentSearches.tsx
+++ src/components/search/RecentSearches.tsx
@@ -1,19 +1,24 @@
 function RecentSearches() {
   return (
     <div className='flex flex-col items-start gap-10 mt-10 mb-10'>
-      <div className='flex flex-row justify-between items-center'>
+      <div className='flex flex- justify-between items-center w-full'>
         <span className='text-black-700 font-pretendard pretendard-2xl-medium'>최근 검색어</span>
-        <span className='text-state-error font-pretendard pretendard-lg-semibold'>모두 지우기</span>
+        <button className='text-state-error font-pretendard pretendard-lg-semibold bg-transparent border-none cursor-pointer' type='submit'>
+          모두 지우기
+        </button>
       </div>
-      <div className='flex flex-col items-start gap-4 text-black-300 font-pretendard pretendard-2xl-regular'>
-        <div className='flex-row items-start gap-4'>
-          <span className='flex flex-col justify-center items-center p-3 px-[14px] gap-2 rounded-[22px] bg-background-100'>꿈</span>
-          <span className='flex flex-col justify-center items-center p-3 px-[14px] gap-2 rounded-[22px] bg-background-100'>#나아가야할때</span>
-          <span className='flex flex-col justify-center items-center p-3 px-[14px] gap-2 rounded-[22px] bg-background-100'>꿈을이루고싶을때</span>
-        </div>
-        <div className='flex-row items-start gap-4'>
-          <span className='flex flex-col justify-center items-center p-3 px-[14px] gap-2 rounded-[22px] bg-background-100'>슬픔</span>
-          <span className='flex flex-col justify-center items-center p-3 px-[14px] gap-2 rounded-[22px] bg-background-100'>응원받고싶을때</span>
+      <div className='flex flex-col items-start gap-10'>
+        <div className='flex flex-row flex-wrap items-start gap-4 text-black-300 font-pretendard pretendard-2xl-regular'>
+          <span className='flex justify-center items-center p-3 px-[14px] gap-2 rounded-[22px] bg-background-100'>꿈</span>
+          <span className='flex justify-center items-center p-3 px-[14px] gap-2 rounded-[22px] bg-background-100'>#나아가야할때</span>
+          <span className='flex justify-center items-center p-3 px-[14px] gap-2 rounded-[22px] bg-background-100'>꿈을이루고싶을때</span>
+          <span className='flex justify-center items-center p-3 px-[14px] gap-2 rounded-[22px] bg-background-100'>슬픔</span>
+          <span className='flex justify-center items-center p-3 px-[14px] gap-2 rounded-[22px] bg-background-100'>응원받고싶을때</span>
+          <span className='flex justify-center items-center p-3 px-[14px] gap-2 rounded-[22px] bg-background-100'>응원받고싶을때</span>
+          <span className='flex justify-center items-center p-3 px-[14px] gap-2 rounded-[22px] bg-background-100'>응원받고싶을때</span>
+          <span className='flex justify-center items-center p-3 px-[14px] gap-2 rounded-[22px] bg-background-100'>응원받고싶을때</span>
+          <span className='flex justify-center items-center p-3 px-[14px] gap-2 rounded-[22px] bg-background-100'>응원받고싶을때</span>
+          <span className='flex justify-center items-center p-3 px-[14px] gap-2 rounded-[22px] bg-background-100'>응원받고싶을때</span>
         </div>
       </div>
     </div>
--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -4,7 +4,7 @@ import SEARCH_ICON from '../../../public/md.svg';
 function SearchBar() {
   return (
     <form className='flex justify-between items-center h-20 mt-6 py-[26px] gap-[10px] border-b-[4px] border-blue-800'>
-      <input className='text-black-700 text-center font-pretendard pretendard-2xl-regular' placeholder='검색어를 입력해주세요.' />
+      <input className='flex text-left text-black-700 text-center font-pretendard pretendard-2xl-regular' placeholder='검색어를 입력해주세요.' />
       <button type='submit' className='flex justify-center items-center bg-transparent'>
         <Image className='w-9 h-9' src={SEARCH_ICON} alt='검색 아이콘' />
       </button>
