b0591d61cebb4c7588b5d17d4142a63e33485edd
NEWJIN <winniepooh00@naver.com>
2024-07-11 23:42:30 +0900

    FE-58 :lips: 댓글 카드 관련 인터페이스, 스타일 분리

--- src/components/Card/CommentCard.tsx
+++ src/components/Card/CommentCard.tsx
@@ -1,54 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
-
-interface CommentCardProps {
-  status: 'edit' | 'complete';
-}
-
-const sizeStyles = {
-  sm: 'w-[360px] h-[130px]',
-  md: 'md:w-[384px] md:h-[162px]',
-  lg: 'lg:w-[640px] lg:h-[176px]',
-};
-
-const textSizeStyles = {
-  sm: {
-    name: 'text-xs',
-    time: 'text-xs',
-    action: 'text-xs',
-    content: 'text-sm',
-  },
-  md: {
-    name: 'md:text-sm',
-    time: 'md:text-sm',
-    action: 'md:text-sm',
-    content: 'md:text-base',
-  },
-  lg: {
-    name: 'lg:text-base',
-    time: 'lg:text-base',
-    action: 'lg:text-base',
-    content: 'lg:text-xl',
-  },
-};
-
-const gapStyles = {
-  sm: 'gap-2',
-  md: 'md:gap-3',
-  lg: 'lg:gap-4',
-};
-
-const paddingStyles = {
-  sm: 'py-4 px-6',
-  md: 'md:p-6',
-  lg: 'lg:py-[35px] lg:px-6',
-};
-
-const contentWidthStyles = {
-  sm: 'w-[248px]',
-  md: 'md:w-[272px]',
-  lg: 'lg:w-[528px]',
-};
+import { CommentCardProps } from '@/types/CommentCardTypes';
+import { sizeStyles, textSizeStyles, gapStyles, paddingStyles, contentWidthStyles } from '@/styles/CommentCardStyles';
 
 function CommentCard({ status }: CommentCardProps) {
   return (
