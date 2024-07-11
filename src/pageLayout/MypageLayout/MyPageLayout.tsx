9ea9eff66b247f1ba7ffe13e2d5e5ad3118cc7cf
전유민 <beforeyum@jeon-yumin-ui-noteubug.local>
2024-07-11 11:14:33 +0900

    FE-28 :rotating_light: build test 오류 수정

--- src/pageLayout/MypageLayout/MyPageLayout.tsx
+++ src/pageLayout/MypageLayout/MyPageLayout.tsx
@@ -1,16 +1,11 @@
 function MyPageLayout() {
   return (
     <div className='bg-background-100 w-full relative h-dvh'>
-      <div className='bg-background-100 w-full h-[200px]' />
+      <div className='bg-background-100 w-full h-[200px]'></div>
       <div className='w-full h-[1900px] flex flex-col items-center bg-blue-100 rounded-3xl relative shadow-3xl'>
         {/* 프로필 영역 */}
         <div className='w-[130px] h-[240px] flex flex-col justify-center items-center absolute top-[-50px]'>
-          <div className='w-[120px] h-[120px] rounded-full bg-state-error' />
-          {/* <img
-            className='w-[120px] h-[120px] rounded-[50%]'
-            alt='프로필 사진'
-            src='https://mblogthumb-phinf.pstatic.net/MjAyMjAxMjVfMjg0/MDAxNjQzMTAyOTg0Nzgz.sLrqOJ2S3r6pLboUm5yJTjB_JECC0zO9Tt3y_h86aJcg.w5VER_KDRAW3yRq8-nypsm2aGmKurM5YieSFcr1Vg0Qg.JPEG.minziminzi128/IMG_7374.JPG?type=w800'
-          /> */}
+          <div className='w-[120px] h-[120px] rounded-full bg-state-error'></div>
           <p className='mt-4 mb-6'>선글라스 고양이</p>
           <div className='w-[100px] h-12 pl-4 pr-3.5 py-1.5 bg-zinc-100 rounded-[100px] justify-start items-center gap-1.5 inline-flex'>
             <div className="text-neutral-400 text-xl font-medium font-['Pretendard'] leading-loose">로그아웃</div>
