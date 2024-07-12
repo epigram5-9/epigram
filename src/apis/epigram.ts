d37c5f9d8013c0caeb4c3d82dcdb0704ce20f256
우지석 <jisurk@gmail.com>
2024-07-12 11:48:05 +0900

    FE-43⚡ ️axios 에러 핸들링 추가

--- src/apis/epigram.ts
+++ src/apis/epigram.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { GetEpigramResponseType, GetEpigramRequestType } from '@/schema/epigram';
 
 const BASE_URL = 'https://fe-project-epigram-api.vercel.app/5-9';
@@ -6,16 +6,35 @@ const BASE_URL = 'https://fe-project-epigram-api.vercel.app/5-9';
 const getEpigram = async (request: GetEpigramRequestType): Promise<GetEpigramResponseType> => {
   const { id } = request;
   const TOKEN =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjIsInRlYW1JZCI6IjUtOSIsInNjb3BlIjoiYWNjZXNzIiwiaWF0IjoxNzIwNjc2MDUzLCJleHAiOjE3MjA2Nzc4NTMsImlzcyI6InNwLWVwaWdyYW0ifQ.7vUUbzQWvjJudUF3g_NLJBh6fJG9gF3S8AgaRxga0bk';
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjIsInRlYW1JZCI6IjUtOSIsInNjb3BlIjoiYWNjZXNzIiwiaWF0IjoxNzIwNzUxNTM2LCJleHAiOjE3MjA3NTMzMzYsImlzcyI6InNwLWVwaWdyYW0ifQ.7TTCMekxh9_J9O0jPAgIlZmYdUMSGGVf7qkiYJqM9rk';
 
-  const response = await axios.get(`${BASE_URL}/epigrams/${id}`, {
-    headers: {
-      // NOTE : 임시로 token을 상수로 선언해서 사용, 토큰관리를 어떻게할지 결정되면 수정 예정
-      Authorization: `Bearer ${TOKEN}`,
-      'Content-Type': 'application/json',
-    },
-  });
-  return response.data;
+  try {
+    const response = await axios.get(`${BASE_URL}/epigrams/${id}`, {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+        'Content-Type': 'application/json',
+      },
+    });
+    return response.data;
+  } catch (error) {
+    // 에러가 Axios에러인지 확인
+    if (axios.isAxiosError(error)) {
+      const axiosError = error as AxiosError;
+      if (axiosError.response) {
+        // 서버에 요청 성공, 응답으로 200번대 이외의 상태를 응답으로 받았을때
+        throw new Error(`API 에러: ${axiosError.response.status}`);
+      } else if (axiosError.request) {
+        // 요청이 이루어졌으나 응답을 받지 못한 경우
+        throw new Error('서버로부터 응답을 받지 못했습니다.');
+      } else {
+        // 요청 설정 중에 오류가 발생한 경우
+        throw new Error('요청 설정 중 오류가 발생했습니다.');
+      }
+    } else {
+      // axios 에러가 아닌 경우
+      throw new Error('예상치 못한 오류가 발생했습니다.');
+    }
+  }
 };
 
 export default getEpigram;
