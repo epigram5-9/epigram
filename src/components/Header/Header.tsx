c3f8cc3e9e3ed32a1d87fdee05c846ff7bfb8520
임수혁 <skilldoy2@naver.com>
2024-07-12 13:20:51 +0900

    FE-50 ✨styles: 주석 추추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,8 +9,12 @@ import SEARCH_ICON from '../../../public/icon/search-icon.svg';
 //TODO 네비게이션 바를 나타내는 컴포넌트 입니다.
 //TODO 상위 컴포넌트에서 Props를 받아 원하는 스타일을 보여줍니다.
 //TODO 사용 예시
-//TODO <Header icon='back' routerPage='원하는 페이지 주소' isLogo={false} insteadOfLogo='프로필 수정' isProfileIcon={false} isButton textInButton='저장' onClick={동작할 함수} />
+//TODO <Header icon='back' routerPage='원하는 페이지 주소' isLogo={false} insteadOfLogo='센터 텍스트' isProfileIcon={false} isButton textInButton='버튼 텍스트' onClick={동작할 함수} />
 //TODO <Header icon='search' routerPage='/search' isLogo insteadOfLogo='' isProfileIcon isButton={false} textInButton='' onClick={() => {}} />;
+//TODO icon: 'back'을 사용할 경우 routerPage의 값을 무조건 지정해줘야 합니다.
+//TODO isLogo={false}일 경우 insteadOfLogo의 값을 무조건 지정해줘야 합니다.
+//TODO isButton 일 경우 textInButton의 값을 무조건 지정해줘야 합니다.
+//TODO 중앙을 제외한 한 쪽에만 버튼이 있는 경우 스타일이 깨질 수 있으니 둘 다 없거나, 둘 다 있어야 합니다.
 
 interface HeaderProps {
   icon: 'back' | 'search' | '';
@@ -31,27 +35,28 @@ function Header({ isLogo, icon, insteadOfLogo, isButton, isProfileIcon, textInBu
   };
 
   return (
-    <nav className='bg-white h-13 px-6 md:px-28 lg:px-30 py-4 md:py-[19px] lg:py-[26px]'>
+    <nav className='bg-white h-13 px-6 py-4 md:px-28 md:py-5 lg:px-30 lg:py-6'>
       <div className='container flex justify-between items-center'>
         {icon === 'back' && (
-          <button className='w-5 lg:w-9 h-5 lg:h-9' type='button' onClick={() => navigateTo(routerPage)} aria-label='뒤로가기 버튼'>
+          <button className='w-5 h-5 lg:w-9 lg:h-9' type='button' onClick={() => navigateTo(routerPage)} aria-label='뒤로가기 버튼'>
             <Image src={ARROW_LEFT_ICON} alt='뒤로가기 버튼 이미지' />
           </button>
         )}
         {icon === 'search' && (
-          <button className='w-5 lg:w-9 h-5 lg:h-9' type='button' onClick={() => navigateTo('/search')} aria-label='검색 버튼'>
+          <button className='w-5 h-5 lg:w-9 lg:h-9' type='button' onClick={() => navigateTo('/search')} aria-label='검색 버튼'>
             <Image src={SEARCH_ICON} alt='검색 버튼 이미지' />
           </button>
         )}
-        {isLogo && (
+        {isLogo ? (
           <button className='flex items-center gap-2' type='button' onClick={() => navigateTo('/')} aria-label='홈으로 이동'>
-            <Image className='w-5 lg:w-9 h-5 lg:h-9' src={LOGO_ICON} alt='logo' />
+            <Image className='w-5 h-5 lg:w-9 lg:h-9' src={LOGO_ICON} alt='logo' />
             <span className='text-black-700 text-6 lg:text-[26px] leading-6 font-bold'>Epigram</span>
           </button>
+        ) : (
+          <span className='text-black-700 text-6 lg:text-[26px] leading-6 font-bold'>{insteadOfLogo}</span>
         )}
-        {!isLogo && <span className='text-black-700 text-6 lg:text-[26px] leading-6 font-bold'>{insteadOfLogo}</span>}
         {isProfileIcon && (
-          <button className='w-5 lg:w-9 h-5 lg:h-9' type='button' onClick={() => navigateTo('/mypage')} aria-label='프로필 페이지로 이동'>
+          <button className='w-5 h-5 lg:w-9 lg:h-9' type='button' onClick={() => navigateTo('/mypage')} aria-label='프로필 페이지로 이동'>
             <Image src={PROFILE_ICON} alt='프로필 이미지' />
           </button>
         )}
