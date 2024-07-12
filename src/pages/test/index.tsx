afd6826ca1e4aa609111cef4e17a2f909cd75f46
임수혁 <skilldoy2@naver.com>
2024-07-12 13:22:59 +0900

    FE-5050 ✨test: 테스트 코드

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -3,10 +3,10 @@ import Header from '@/components/Header/Header';
 
 function test() {
   const testOnclick = () => {
-    alert('테스트');
+    alert('테스트 코드');
   };
 
-  return <Header icon='back' routerPage='/search' isLogo={false} insteadOfLogo='프로필 수정' isProfileIcon={false} isButton textInButton='완료' onClick={testOnclick} />;
+  return <Header icon='search' routerPage='/search' isLogo insteadOfLogo='' isProfileIcon={false} isButton textInButton='테스트' onClick={testOnclick} />;
 }
 
 export default test;
