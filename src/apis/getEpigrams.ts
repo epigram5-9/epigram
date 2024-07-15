346ecf627c95432ee4aad90b9d7803451580f54a
임수혁 <skilldoy2@naver.com>
2024-07-15 13:42:09 +0900

    FE-522 ✨fix:  default export로 변경

--- src/apis/getEpigrams.ts
+++ src/apis/getEpigrams.ts
@@ -1,12 +1,14 @@
-import httpClient from '.';
 import { GetEpigramsParamsType, GetEpigramsResponseType, GetEpigramsResponse } from '@/schema/epigrams';
+import httpClient from '.';
 
 const TEAM_ID = '5-9';
 
-export const getEpigrams = async (params: GetEpigramsParamsType): Promise<GetEpigramsResponseType> => {
+const getEpigrams = async (params: GetEpigramsParamsType): Promise<GetEpigramsResponseType> => {
   const response = await httpClient.get(`/${TEAM_ID}/epigrams`, { params });
 
   // 데이터 일치하는지 확인
   const parsedResponse = GetEpigramsResponse.parse(response.data);
   return parsedResponse;
 };
+
+export default getEpigrams;
