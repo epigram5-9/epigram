0cb7403f4cabf73187749e22152f7bdd04b3ebb1
임수혁 <skilldoy2@naver.com>
2024-07-15 13:40:39 +0900

    FE-52 ✨feat: getEpigrams api 작성

--- src/apis/getEpigrams.ts
+++ src/apis/getEpigrams.ts
@@ -1,3 +1,12 @@
-function getEpigrams() {}
+import httpClient from '.';
+import { GetEpigramsParamsType, GetEpigramsResponseType, GetEpigramsResponse } from '@/schema/epigrams';
 
-export default getEpigrams;
+const TEAM_ID = '5-9';
+
+export const getEpigrams = async (params: GetEpigramsParamsType): Promise<GetEpigramsResponseType> => {
+  const response = await httpClient.get(`/${TEAM_ID}/epigrams`, { params });
+
+  // 데이터 일치하는지 확인
+  const parsedResponse = GetEpigramsResponse.parse(response.data);
+  return parsedResponse;
+};
