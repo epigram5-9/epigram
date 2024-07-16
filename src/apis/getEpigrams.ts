fbd87c12ba76a548c989b594f0ff7b5a0d1c304a
임수혁 <skilldoy2@naver.com>
2024-07-16 19:54:37 +0900

    FE-52 ✨feat: BaseUrl, TeamID 상수 추가 및 axios baseTRL 수정

--- src/apis/getEpigrams.ts
+++ src/apis/getEpigrams.ts
@@ -1,10 +1,8 @@
 import { GetEpigramsParamsType, GetEpigramsResponseType, GetEpigramsResponse } from '@/schema/epigrams';
 import httpClient from '.';
 
-const TEAM_ID = '5-9';
-
 const getEpigrams = async (params: GetEpigramsParamsType): Promise<GetEpigramsResponseType> => {
-  const response = await httpClient.get(`/${TEAM_ID}/epigrams`, { params });
+  const response = await httpClient.get(`/api/epigrams`, { params });
 
   // 데이터 일치하는지 확인
   const parsedResponse = GetEpigramsResponse.parse(response.data);
--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import qs from 'qs';
 
+const BASE_URL = 'https://fe-project-epigram-api.vercel.app';
+const TEAM_ID = '5-9';
+
 const httpClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  baseURL: `${BASE_URL}/${TEAM_ID}/`,
   headers: { 'Content-Type': 'application/json' },
   paramsSerializer: (parameters) => qs.stringify(parameters, { arrayFormat: 'repeat', encode: false }),
 });
