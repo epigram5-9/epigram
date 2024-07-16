b4bf439f94d02f3550e3eb7503cb26551ee7ee8e
임수혁 <skilldoy2@naver.com>
2024-07-16 21:42:25 +0900

    FE-52 ✨fix: api GET요청 주소 수정('epigrams' -> '/epigrams')

--- src/apis/getEpigrams.ts
+++ src/apis/getEpigrams.ts
@@ -2,7 +2,7 @@ import { GetEpigramsParamsType, GetEpigramsResponseType, GetEpigramsResponse } f
 import httpClient from '.';
 
 const getEpigrams = async (params: GetEpigramsParamsType): Promise<GetEpigramsResponseType> => {
-  const response = await httpClient.get(`epigrams`, { params });
+  const response = await httpClient.get(`/epigrams`, { params });
 
   // 데이터 일치하는지 확인
   const parsedResponse = GetEpigramsResponse.parse(response.data);
