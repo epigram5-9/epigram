fa5851e240576e10cf87f45b43de114203e12fb5
NEWJIN <winniepooh00@naver.com>
2024-07-17 10:02:01 +0900

    FE-25 :sparkles: 환경 변수로 Pixel ID 관리 구현
    환경 변수를 통해 Facebook Pixel ID를 안전하게 관리

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,13 +6,13 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { GoogleAnalytics } from 'nextjs-google-analytics';
 import ReactPixel from 'react-facebook-pixel';
 
-const FB_PIXEL_ID = '1012060337236946';
+const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FB_PIXEL_ID;
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = React.useState(() => new QueryClient());
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== 'undefined' && FB_PIXEL_ID) {
       // Facebook Pixel 초기화 및 페이지 뷰 트래킹
       ReactPixel.init(FB_PIXEL_ID);
       ReactPixel.pageView();
