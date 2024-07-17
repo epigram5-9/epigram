379973ecb138346710c443694cd602174c976a05
우지석 <jisurk@gmail.com>
2024-07-17 12:43:10 +0900

    FE-42💄작성된 댓글 없을때 UI 추가

--- src/components/epigram/Comment/CommentList.tsx
+++ src/components/epigram/Comment/CommentList.tsx
@@ -1,6 +1,7 @@
 import { useEpigramCommentsQuery } from '@/hooks/useEpigramQueryHook';
 import { EpigramCommentProps } from '@/types/epigram.types';
 import CommentItem from './CommentItem';
+import NoComment from './NoComment';
 
 function CommentList({ epigramId, currentUserId }: EpigramCommentProps) {
   const {
@@ -14,11 +15,12 @@ function CommentList({ epigramId, currentUserId }: EpigramCommentProps) {
 
   if (isLoading) return <div>댓글을 불러오는 중...</div>;
   if (error) return <div>에러: {(error as Error).message}</div>;
+  const commentCount = comments?.list.length || 0;
 
   return (
-    <div>
+    <div className='flex flex-col gap-4'>
       <h3 className='text-base lg:text-xl font-semibold'>댓글({comments?.list.length || 0})</h3>
-      {comments?.list.map((comment) => <CommentItem key={comment.id} comment={comment} status={comment.writer.id === currentUserId ? 'edit' : 'complete'} />)}
+      {commentCount > 0 ? comments?.list.map((comment) => <CommentItem key={comment.id} comment={comment} status={comment.writer.id === currentUserId ? 'edit' : 'complete'} />) : <NoComment />}
     </div>
   );
 }
