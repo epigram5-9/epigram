f88755da92362484887af077ee1124c94cd67474
임수혁 <skilldoy2@naver.com>
2024-07-19 00:57:23 +0900

    FE-65 ✨feat: 최근 검색어 기능 구현

--- src/pageLayout/SearchLayout/SearchLayout.tsx
+++ src/pageLayout/SearchLayout/SearchLayout.tsx
@@ -4,12 +4,21 @@ import RecentSearches from '@/components/search/RecentSearches';
 import SearchResults from '@/components/search/SearchResults';
 
 function SearchLayout() {
+  const handleSearch = (search: string) => {
+    if (search.trim() === '') return;
+
+    // 로컬 스토리지 저장
+    const storedSearches = JSON.parse(localStorage.getItem('recentSearches') || '[]');
+    const updatedSearches = [search, ...storedSearches.filter((item: string) => item !== search)].slice(0, 10);
+    localStorage.setItem('recentSearches', JSON.stringify(updatedSearches));
+  };
+
   return (
     <>
       <header />
       <div className='container mx-auto max-w-screen-sm bg-blue-100'>
         <SearchBar />
-        <RecentSearches />
+        <RecentSearches onSearch={handleSearch} />
         <SearchResults />
         <SearchResults />
         <SearchResults />
