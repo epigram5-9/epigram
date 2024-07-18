eb34b5db35977d0cc1eb5e23579622396414f3c1
MOON <muneon96@naver.com>
2024-07-18 20:40:48 +0900

    :sparkles: 회원가입 스키마 정의

---
+++ src/schema/auth.ts
@@ -0,0 +1,20 @@
+import z from 'zod';
+
+const PWD_VALIDATION = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/;
+export const PostSignUpRequest = z
+  .object({
+    email: z.string().min(1, { message: '이메일은 필수 입력입니다.' }).email({ message: '이메일 형식으로 작성해 주세요.' }),
+    password: z
+      .string()
+      .min(1, { message: '비밀번호는 필수 입력입니다.' })
+      .min(8, { message: '비밀번호는 최소 8자 이상입니다.' })
+      .regex(PWD_VALIDATION, { message: '비밀번호는 숫자, 영문, 특수문자로만 가능합니다.' }),
+    passwordConfirmation: z.string().min(1, { message: '비밀번호 확인을 입력해주세요.' }),
+    nickname: z.string().min(1, { message: '닉네임은 필수 입력입니다.' }).max(20, { message: '닉네임은 최대 20자까지 가능합니다.' }),
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: '비밀번호가 일치하지 않습니다.',
+    path: ['passwordConfirmation'],
+  });
+
+export type PostSignUpRequestType = z.infer<typeof PostSignUpRequest>;
