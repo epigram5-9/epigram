c5afae8a89cdd1686e532b172e410a51ede5c3a8
임수혁 <skilldoy2@naver.com>
2024-07-20 19:44:23 +0900

    FE-65 ✨fix: 주석 수정

--- src/pageLayout/SearchLayout/SearchLayout.tsx
+++ src/pageLayout/SearchLayout/SearchLayout.tsx
@@ -3,7 +3,7 @@ import SearchBar from '@/components/search/SearchBar';
 import RecentSearches from '@/components/search/RecentSearches';
 import SearchResults from '@/components/search/SearchResults';
 
-//TODO 로그인한 사용자에 따라서 최근 검색어를 관리할 수 있도록 추후에 수정
+// TODO 로그인한 사용자에 따라서 최근 검색어를 관리할 수 있도록 추후에 수정
 // NOTE useEffect 훅이 두 번 실행 되는 문제를 해결하기 위해 isInitialMount 선언하여 관리
 
 function SearchLayout() {
@@ -19,19 +19,19 @@ function SearchLayout() {
     });
   };
 
-  // 모두지우기 클릭 시 최근 검색어 삭제
+  // 모두지우기 클릭 시 저장된 최근 검색어 삭제
   const handleClearAll = () => {
     setSearches([]);
     localStorage.removeItem('recentSearches');
   };
 
-  // 컴포넌트가 처음 렌더링 될 때 최근 검색어 불러옴
+  // 컴포넌트가 처음 렌더링 될 때 저장된 최근 검색어 불러오기
   useEffect(() => {
     const storedSearches = JSON.parse(localStorage.getItem('recentSearches') || '[]');
     setSearches(storedSearches);
   }, []);
 
-  // 검색어 상태가 변경될 때 로컬 스토리지 업데이트
+  // 검색어 상태(searches)가 변경될 때 로컬 스토리지 업데이트
   useEffect(() => {
     if (isInitialMount.current) {
       isInitialMount.current = false;
