188f9cea78ee5b0b789da331fedcf392bacf875a
임수혁 <skilldoy2@naver.com>
2024-07-20 13:08:16 +0900

    FE-65 ✨feat:  focus될 때 placeholder 사라지게 수정 및 UI 수정

--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -8,9 +8,18 @@ interface SearchBarProps {
 
 function SearchBar({ onSearch }: SearchBarProps) {
   const [searchInput, setSearchInput] = useState<string>('');
+  const [isFocused, setIsFocused] = useState<boolean>(false);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchInput(e.target.value);
+  };
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
   };
 
   const handleSubmit = (e: React.FormEvent) => {
@@ -24,10 +33,12 @@ function SearchBar({ onSearch }: SearchBarProps) {
   return (
     <form onSubmit={handleSubmit} className='flex justify-between items-center h-[52px] md:h-15 lg:h-20 mt-6 mx-6 py-4 md:py-[19px] lg:py-[26px] gap-[10px] border-b-[4px] border-blue-800'>
       <input
-        className='flex text-left text-black-700 text-center font-pretendard pretendard-lg-regular md:pretendard-xl-regular lg:pretendard-2xl-regular'
-        placeholder='검색어를 입력해주세요.'
+        className='flex-grow border-none text-left text-black-700 font-pretendard pretendard-lg-regular md:pretendard-xl-regular lg:pretendard-2xl-regular focus:outline-none'
+        placeholder={isFocused ? '' : '검색어를 입력해주세요.'}
         value={searchInput}
         onChange={handleChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
       />
       <button type='submit' className='flex justify-center items-center bg-transparent'>
         <Image className='w-5 lg:w-9 h-5 lg:h-9' src={SEARCH_ICON} alt='검색 아이콘' />
