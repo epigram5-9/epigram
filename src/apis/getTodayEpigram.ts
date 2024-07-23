96520cedd3dfee49b42d5b3bef5c334fbb036103
NEWJIN <winniepooh00@naver.com>
2024-07-23 18:53:44 +0900

    FE-15 :fire: api 함수 불필요한 부분 제거

--- src/apis/getTodayEpigram.ts
+++ src/apis/getTodayEpigram.ts
@@ -1,24 +1,9 @@
-import { EpigramType } from '@/schema/todayepigram';
-import { AxiosError } from 'axios';
+import type { EpigramType } from '@/schema/todayepigram';
 import httpClient from './index';
 
 const getTodayEpigram = async (): Promise<EpigramType> => {
-  try {
-    const response = await httpClient.get<EpigramType>('/epigrams/today');
-    return response.data;
-  } catch (error) {
-    if (error instanceof AxiosError) {
-      if (error.response) {
-        throw new Error('에피그램 조회 요청 처리 중 문제가 발생했습니다.');
-      } else if (error.request) {
-        throw new Error('서버 응답을 받지 못했습니다. 잠시 후 다시 시도해 주세요.');
-      } else {
-        throw new Error('에피그램 조회 요청을 처리하는 동안 문제가 발생했습니다.');
-      }
-    } else {
-      throw new Error('알 수 없는 오류가 발생했습니다.');
-    }
-  }
+  const response = await httpClient.get('/epigrams/today');
+  return response.data;
 };
 
 export default getTodayEpigram;
