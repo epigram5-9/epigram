b52c4f5c49167c9563197b4bacf4582ba99c5e85
NEWJIN <winniepooh00@naver.com>
2024-07-23 16:21:30 +0900

    FE-53 :sparkles: 오늘의 감정 조회 함수 적용

--- src/components/Emotion/EmotionSelector.tsx
+++ src/components/Emotion/EmotionSelector.tsx
@@ -1,15 +1,16 @@
 /*
   여러 개의 EmotionIconCard를 관리합니다.
-  사용자 인터페이스에 필요한 상호 작용 로직을 포함합니다.
- */
+  오늘의 감정 조회 함수를 통해 감정 카드의 상태를 변경합니다.
+  감정 카드 클릭 핸들러를 통해 오늘의 감정을 저장합니다.
+  */
 
-import React, { useState } from 'react';
-import EmotionIconCard from '@/components/Emotion/EmotionCard';
+import React, { useState, useEffect } from 'react';
 import useMediaQuery from '@/hooks/useMediaQuery';
+import EmotionIconCard from '@/components/Emotion/EmotionCard';
 import { EmotionType, EmotionState } from '@/types/EmotionTypes';
 import postEmotion from '@/apis/postEmotion';
-import translateEmotion from '@/utils/emotionMap';
-import type { PostEmotionRequestType } from '@/schema/emotion';
+import getEmotion from '@/apis/getEmotion';
+import EmotionSaveToast from './EmotionSaveToast';
 
 // EmotionSelector 컴포넌트 함수 선언
 function EmotionSelector() {
@@ -24,6 +25,30 @@ function EmotionSelector() {
     슬픔: 'Default',
     분노: 'Default',
   });
+
+  const [selectedEmotion, setSelectedEmotion] = useState<EmotionType | null>(null);
+
+  // getEmotion 함수로 오늘의 감정 조회
+  useEffect(() => {
+    const fetchEmotion = async () => {
+      try {
+        const emotion = await getEmotion();
+        if (emotion) {
+          setStates((prevStates) => ({
+            ...prevStates,
+            [emotion]: 'Clicked',
+          }));
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          // eslint-disable-next-line
+          console.error(error.message);
+        }
+      }
+    };
+
+    fetchEmotion();
+  }, []);
 
   // 감정 카드 클릭 핸들러
   const handleCardClick = async (iconType: EmotionType) => {
@@ -45,12 +70,10 @@ function EmotionSelector() {
       return newStates;
     });
 
-    const englishEmotion = translateEmotion(iconType);
-    const request: PostEmotionRequestType = { emotion: englishEmotion };
-
-    // postEmotion 함수 호출
+    // postEmotion 함수로 오늘의 감정 저장
     try {
-      await postEmotion(request);
+      await postEmotion(iconType);
+      setSelectedEmotion(iconType);
     } catch (error) {
       if (error instanceof Error) {
         // eslint-disable-next-line
@@ -71,11 +94,14 @@ function EmotionSelector() {
   }
 
   return (
-    <div className={`justify-start items-start inline-flex ${containerClass}`}>
-      {(['감동', '기쁨', '고민', '슬픔', '분노'] as const).map((iconType) => (
-        <EmotionIconCard key={iconType} iconType={iconType} size={cardSize} state={states[iconType]} onClick={() => handleCardClick(iconType)} />
-      ))}
-    </div>
+    <>
+      <div className={`justify-start items-start inline-flex ${containerClass}`}>
+        {(['감동', '기쁨', '고민', '슬픔', '분노'] as const).map((iconType) => (
+          <EmotionIconCard key={iconType} iconType={iconType} size={cardSize} state={states[iconType]} onClick={() => handleCardClick(iconType)} />
+        ))}
+      </div>
+      {selectedEmotion && <EmotionSaveToast iconType={selectedEmotion} />}
+    </>
   );
 }
 
