4c43bc082246daa4095dabc4e4c6d4bca9ee9b0b
NEWJIN <winniepooh00@naver.com>
2024-07-23 16:16:07 +0900

    FE-53 :sparkles: 오늘의 감정 스키마 추가 정의

--- src/schema/emotion.ts
+++ src/schema/emotion.ts
@@ -1,12 +1,17 @@
 import * as z from 'zod';
 
-// Request schema
 export const PostEmotionRequest = z.object({
   emotion: z.enum(['MOVED', 'JOY', 'WORRY', 'SADNESS', 'ANGER']),
 });
 
-// Response schema
 export const PostEmotionResponse = z.object({
+  createdAt: z.coerce.date(),
+  emotion: z.enum(['MOVED', 'JOY', 'WORRY', 'SADNESS', 'ANGER']),
+  userId: z.number(),
+  id: z.number(),
+});
+
+export const GetEmotionResponse = z.object({
   createdAt: z.coerce.date(),
   emotion: z.enum(['MOVED', 'JOY', 'WORRY', 'SADNESS', 'ANGER']),
   userId: z.number(),
@@ -15,3 +20,4 @@ export const PostEmotionResponse = z.object({
 
 export type PostEmotionRequestType = z.infer<typeof PostEmotionRequest>;
 export type PostEmotionResponseType = z.infer<typeof PostEmotionResponse>;
+export type GetEmotionResponseType = z.infer<typeof GetEmotionResponse>;
