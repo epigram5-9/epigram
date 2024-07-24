7b10e6c200d8273ee5623aff6a6e7ee014320993
NEWJIN <winniepooh00@naver.com>
2024-07-24 16:26:52 +0900

    FE-17 :sparkles: 최근 에피그램 더보기 버튼 구현

---
+++ src/components/main/LoadMoreButton.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Image from 'next/image';
+
+interface LoadMoreButtonProps {
+  onClick: () => void;
+}
+
+function LoadMoreButton({ onClick }: LoadMoreButtonProps) {
+  return (
+    <div
+      onClick={onClick}
+      className='h-12 px-[18px] py-3 rounded-[100px] border border-[#cfdbea] justify-center items-center gap-1 inline-flex cursor-pointer'
+      role='button'
+      tabIndex={0}
+      onKeyPress={(e) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+          onClick();
+        }
+      }}
+    >
+      <div className='w-6 h-6 relative' />
+      <Image src='/icon/plus-icon.svg' alt='plus icon' layout='fill' objectFit='contain' />
+      <div className='text-#abb8ce text-sm font-normal font-Pretendard leading-normal'>더보기</div>
+    </div>
+  );
+}
+
+export default LoadMoreButton;
