e141c2c82cc841e031ce450f3e8aa0c47dd47e8f
임수혁 <skilldoy2@naver.com>
2024-07-25 15:49:50 +0900

    FE-74 ✨feat: 검색 결과 기능 구현

--- src/components/search/SearchResults.tsx
+++ src/components/search/SearchResults.tsx
@@ -1,17 +1,56 @@
 import React from 'react';
+import Link from 'next/link';
 
-function SearchResults() {
+interface Tag {
+  name: string;
+  id: number;
+}
+
+interface Epigram {
+  likeCount: number;
+  tags: Tag[];
+  id: number;
+  writerId: number;
+  referenceUrl: string;
+  referenceTitle: string;
+  author: string;
+  content: string;
+}
+
+interface SearchResultsProps {
+  results: {
+    totalCount: number;
+    nextCursor: number;
+    list: Epigram[];
+  } | null;
+}
+
+function SearchResults({ results }: SearchResultsProps) {
+  if (!results) {
+    return <div>검색 결과가 없습니다.</div>;
+  }
+
   return (
-    <div className='flex flex-col py-4 px-6 lg:p-6 gap-2 lg:gap-[16px] border-b border-gray-100'>
-      <div className='flex flex-col gap-1 md:gap-2 lg:gap-6'>
-        <span className='text-black-600 font-iropkeBatang iropke-lg lg:iropke-xl'>오랫동안 꿈을 그리는 사람은 마침내 그 꿈을 닮아 간다.</span>
-        <span className='text-blue-400 font-iropkeBatang iropke-lg lg:iropke-xl'>- 앙드레 말로 -</span>
-      </div>
-      <div className='flex flex-row justify-end gap-3'>
-        <span className='text-blue-400 font-pretendard iropke-lg lg:iropke-xl'>#동기부여</span>
-        <span className='text-blue-400 font-pretendard iropke-lg lg:iropke-xl'>#우울할때</span>
-        <span className='text-blue-400 font-pretendard iropke-lg lg:iropke-xl'>#나아가야할때</span>
-      </div>
+    <div>
+      {results.list.map((item) => (
+        <Link href={`/epigrams/${item.id}`} key={item.id}>
+          <a href={`/epigrams/${item.id}`}>
+            <div key={item.id} className='flex flex-col py-4 px-6 lg:p-6 gap-2 lg:gap-[16px] border-b border-gray-100'>
+              <div className='flex flex-col gap-1 md:gap-2 lg:gap-6'>
+                <span className='text-black-600 font-iropkeBatang iropke-lg lg:iropke-xl'>{item.content}</span>
+                <span className='text-blue-400 font-iropkeBatang iropke-lg lg:iropke-xl'>- {item.author} -</span>
+              </div>
+              <div className='flex flex-row justify-end gap-3'>
+                {item.tags.map((tag) => (
+                  <span key={tag.id} className='text-blue-400 font-pretendard iropke-lg lg:iropke-xl'>
+                    #{tag.name}
+                  </span>
+                ))}
+              </div>
+            </div>
+          </a>
+        </Link>
+      ))}
     </div>
   );
 }
--- src/pageLayout/SearchLayout/SearchLayout.tsx
+++ src/pageLayout/SearchLayout/SearchLayout.tsx
@@ -2,16 +2,18 @@ import React, { useState, useEffect } from 'react';
 import SearchBar from '@/components/search/SearchBar';
 import RecentSearches from '@/components/search/RecentSearches';
 import SearchResults from '@/components/search/SearchResults';
+import { GetEpigramsResponseType } from '@/schema/epigrams';
+import getEpigrams from '@/apis/getEpigrams';
 
 // TODO 로그인한 사용자에 따라서 최근 검색어를 관리할 수 있도록 추후에 수정
-// TODO api, SearchResults 컴포넌트와 연결
 
 function SearchLayout() {
   const [searches, setSearches] = useState<string[]>([]);
   const [currentSearch, setCurrentSearch] = useState<string>('');
+  const [results, setResults] = useState<GetEpigramsResponseType | null>(null);
 
   // 검색어가 제출될 때 작동
-  const handleSearch = (search: string) => {
+  const handleSearch = async (search: string) => {
     setSearches((prevSearches) => {
       // 중복되지 않는 검색어를 최근 검색어에 추가하고 최대 10개로 제한
       const updatedSearches = [search, ...prevSearches.filter((item) => item !== search)].slice(0, 10);
@@ -19,6 +21,14 @@ function SearchLayout() {
       return updatedSearches;
     });
     setCurrentSearch(search);
+
+    // API에서 데이터 가져오기
+    try {
+      const response = await getEpigrams({ keyword: search, limit: 10 }); // TODO 후에 무한스크롤로 구현
+      setResults(response);
+    } catch (error) {
+      setResults(null);
+    }
   };
 
   // 모두지우기 클릭 시 저장된 최근 검색어 삭제
@@ -39,7 +49,7 @@ function SearchLayout() {
       <div className='container mx-auto max-w-screen-sm bg-blue-100'>
         <SearchBar onSearch={handleSearch} currentSearch={currentSearch} />
         <RecentSearches searches={searches} onSearch={handleSearch} onClear={handleClearAll} />
-        <SearchResults />
+        <SearchResults results={results} />
       </div>
     </>
   );
