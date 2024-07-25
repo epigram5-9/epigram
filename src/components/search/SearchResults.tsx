997e13df0b282274de9f44fa9c13a27cf6aeb20d
임수혁 <skilldoy2@naver.com>
2024-07-25 16:25:31 +0900

    FE-74 ✨fix: 중복된 key, href 제거 및 규칙 무시 주석 추가

--- src/components/search/SearchResults.tsx
+++ src/components/search/SearchResults.tsx
@@ -33,9 +33,10 @@ function SearchResults({ results }: SearchResultsProps) {
   return (
     <div>
       {results.list.map((item) => (
-        <Link href={`/epigrams/${item.id}`} key={item.id}>
-          <a href={`/epigrams/${item.id}`}>
-            <div key={item.id} className='flex flex-col py-4 px-6 lg:p-6 gap-2 lg:gap-[16px] border-b border-gray-100'>
+        <Link href={`/epigrams/${item.id}`} key={item.id} passHref>
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <a className='block'>
+            <div className='flex flex-col py-4 px-6 lg:p-6 gap-2 lg:gap-[16px] border-b border-gray-100'>
               <div className='flex flex-col gap-1 md:gap-2 lg:gap-6'>
                 <span className='text-black-600 font-iropkeBatang iropke-lg lg:iropke-xl'>{item.content}</span>
                 <span className='text-blue-400 font-iropkeBatang iropke-lg lg:iropke-xl'>- {item.author} -</span>
