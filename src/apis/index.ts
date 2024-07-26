959459c21e1b742a09cac903c88a58f9fe720bab
Jiseok Woo <115205098+jisurk@users.noreply.github.com>
2024-07-26 17:39:47 +0900

    FE-71♻️ epic브랜치 코드리뷰 반영 (#76)
    * FE-71♻️  token,interceptor 로직 수정
    
    * FE-71♻️  AddEpigram 코드리뷰 반영
    
    * FE-71🔥 테스트용 상세페이지 삭제

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,35 +1,11 @@
 import axios from 'axios';
 import qs from 'qs';
 
-const getToken = () =>
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjUsInRlYW1JZCI6IjUtOSIsInNjb3BlIjoiYWNjZXNzIiwiaWF0IjoxNzIxODAxOTU1LCJleHAiOjE3MjE4MDM3NTUsImlzcyI6InNwLWVwaWdyYW0ifQ.z_QkXSBKp6gsWH7qs_wdNqQcbzIKAiJieihWfY9LZWY';
-
 const httpClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   headers: { 'Content-Type': 'application/json' },
   paramsSerializer: (parameters) => qs.stringify(parameters, { arrayFormat: 'repeat', encode: false }),
 });
-
-httpClient.interceptors.request.use(
-  (config) => {
-    const newConfig = { ...config };
-    const token = getToken();
-    if (token) {
-      newConfig.headers.Authorization = `Bearer ${token}`;
-    }
-
-    if (newConfig.data instanceof FormData) {
-      newConfig.headers['Content-Type'] = 'multipart/form-data';
-    } else {
-      newConfig.headers['Content-Type'] = 'application/json';
-    }
-
-    return newConfig;
-  },
-  (error) => Promise.reject(error),
-);
-
-export default httpClient;
 
 // NOTE: eslint-disable no-param-reassign 미해결로 인한 설정
 httpClient.interceptors.request.use((config) => {
@@ -72,3 +48,5 @@ httpClient.interceptors.response.use(
     return Promise.reject(error);
   },
 );
+
+export default httpClient;
--- src/hooks/useTagManagementHook.ts
+++ src/hooks/useTagManagementHook.ts
@@ -3,23 +3,34 @@ import { UseFormSetValue, UseFormGetValues, UseFormSetError } from 'react-hook-f
 import { AddEpigramFormType } from '@/schema/addEpigram';
 
 // NOTE: setError메서드로 FormField에 에러 설정 가능
-const useTagManagement = (setValue: UseFormSetValue<AddEpigramFormType>, getValues: UseFormGetValues<AddEpigramFormType>, setError: UseFormSetError<AddEpigramFormType>) => {
+const useTagManagement = ({
+  setValue,
+  getValues,
+  setError,
+}: {
+  setValue: UseFormSetValue<AddEpigramFormType>;
+  getValues: UseFormGetValues<AddEpigramFormType>;
+  setError: UseFormSetError<AddEpigramFormType>;
+}) => {
   const [currentTag, setCurrentTag] = useState('');
 
   const handleAddTag = () => {
-    if (currentTag && currentTag.length <= 10) {
-      const currentTags = getValues('tags') || [];
-      if (currentTags.length < 3) {
-        // NOTE: 중복된 태그가 있는지 확인
-        if (currentTags.includes(currentTag)) {
-          setError('tags', { type: 'manual', message: '이미 저장된 태그입니다.' });
-        } else {
-          setValue('tags', [...currentTags, currentTag]);
-          setCurrentTag('');
-          setError('tags', { type: 'manual', message: '' });
-        }
-      }
+    if (!currentTag || currentTag.length > 10) {
+      return;
     }
+    const currentTags = getValues('tags') || [];
+
+    if (currentTags.length >= 3) {
+      return;
+    }
+    if (currentTags.includes(currentTag)) {
+      setError('tags', { type: 'manual', message: '이미 저장된 태그입니다.' });
+      return;
+    }
+
+    setValue('tags', [...currentTags, currentTag]);
+    setCurrentTag('');
+    setError('tags', { type: 'manual', message: '' });
   };
 
   const handleRemoveTag = (tagToRemove: string) => {
