3a780f1fa7628da9aa257a59f69af84cbc36f59e
Woojiseok <jisurk154@naver.com>
2024-07-26 13:26:37 +0900

    FE-71♻️  token,interceptor 로직 수정

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,33 +1,11 @@
 import axios from 'axios';
 import qs from 'qs';
 
-const getToken = () =>
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjUsInRlYW1JZCI6IjUtOSIsInNjb3BlIjoiYWNjZXNzIiwiaWF0IjoxNzIxODAxOTU1LCJleHAiOjE3MjE4MDM3NTUsImlzcyI6InNwLWVwaWdyYW0ifQ.z_QkXSBKp6gsWH7qs_wdNqQcbzIKAiJieihWfY9LZWY';
-
 const httpClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   headers: { 'Content-Type': 'application/json' },
   paramsSerializer: (parameters) => qs.stringify(parameters, { arrayFormat: 'repeat', encode: false }),
 });
-
-httpClient.interceptors.request.use(
-  (config) => {
-    const newConfig = { ...config };
-    const token = getToken();
-    if (token) {
-      newConfig.headers.Authorization = `Bearer ${token}`;
-    }
-
-    if (newConfig.data instanceof FormData) {
-      newConfig.headers['Content-Type'] = 'multipart/form-data';
-    } else {
-      newConfig.headers['Content-Type'] = 'application/json';
-    }
-
-    return newConfig;
-  },
-  (error) => Promise.reject(error),
-);
 
 export default httpClient;
 
