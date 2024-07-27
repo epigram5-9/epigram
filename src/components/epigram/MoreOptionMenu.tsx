7e0b764a727bec0621edd7f5354869bf2bbfe34f
우지석 <jisurk@gmail.com>
2024-07-27 14:52:18 +0900

    FE-45💄 에피그램 삭제 모달 추가

--- src/components/epigram/MoreOptionMenu.tsx
+++ src/components/epigram/MoreOptionMenu.tsx
@@ -1,7 +1,46 @@
 import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import useDeleteEpigram from '@/hooks/useDeleteEpigramHook';
+import { AlertDialogCancel } from '@radix-ui/react-alert-dialog';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '../ui/dropdown-menu';
+import { toast } from '../ui/use-toast';
+import { AlertDialog, AlertDialogAction, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from '../ui/alert-dialog';
 
-function MoreOptionsMenu() {
+interface MoreOptionsMenuProps {
+  epigram: number;
+}
+
+function MoreOptionsMenu({ epigram }: MoreOptionsMenuProps) {
+  const router = useRouter();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const { mutate: deleteEpigram } = useDeleteEpigram({
+    onSuccess: (data) => {
+      toast({
+        title: '삭제 완료',
+        description: `에피그램 ${data.id}가 성공적으로 삭제되었습니다.`,
+      });
+      router.push('/');
+    },
+    onError: () => {
+      toast({
+        title: '삭제 실패',
+        description: '에피그램 삭제 중 오류가 발생했습니다.',
+        variant: 'destructive',
+      });
+    },
+  });
+
+  const handleDeleteClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleDeleteConfirm = () => {
+    deleteEpigram(epigram);
+    setIsModalOpen(false);
+  };
+
   return (
     <div className='relative'>
       <DropdownMenu>
@@ -13,9 +52,29 @@ function MoreOptionsMenu() {
         {/* NOTE: width를 조정할려면 Dropdown컴포넌트에서 min-w 수정 필요 */}
         <DropdownMenuContent className='absolute top-[-36px] left-5 z-10 bg-white'>
           <DropdownMenuItem className='hover:text-illust-blue cursor-pointer text-center'>수정</DropdownMenuItem>
-          <DropdownMenuItem className='hover:text-illust-blue cursor-pointer text-center'>삭제</DropdownMenuItem>
+          <DropdownMenuItem className='hover:text-red-500 cursor-pointer text-center' onClick={handleDeleteClick}>
+            삭제
+          </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
+      <AlertDialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+        <AlertDialogContent className='w-[384px] bg-white z-50 flex flex-col items-center rounded-lg'>
+          <AlertDialogHeader>
+            <AlertDialogTitle>에피그램을 삭제하시겠어요?</AlertDialogTitle>
+          </AlertDialogHeader>
+          <AlertDialogFooter className='flex flex-row gap-2'>
+            <AlertDialogCancel
+              className='w-28 px-4 bg-blue-200 rounded-lg
+            '
+            >
+              취소
+            </AlertDialogCancel>
+            <AlertDialogAction onClick={handleDeleteConfirm} className='w-28 bg-state-error text-white rounded-lg'>
+              삭제하기
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 }
