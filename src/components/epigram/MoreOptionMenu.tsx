5c470774f0028ddc778bfa79ecc16a74116537f5
우지석 <jisurk@gmail.com>
2024-07-28 12:06:16 +0900

    FE-78💄 미트볼아이콘 ui수정

--- src/components/epigram/MoreOptionMenu.tsx
+++ src/components/epigram/MoreOptionMenu.tsx
@@ -46,7 +46,7 @@ function MoreOptionsMenu({ epigram }: MoreOptionsMenuProps) {
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button type='button'>
-            <Image src='/meatballIcon.svg' alt='더 보기 아이콘' width={36} height={36} /> <Image src='/meatballIcon.svg' alt='더 보기 아이콘' width={36} height={36} />
+            <Image src='/meatballIcon.svg' alt='더 보기 아이콘' width={36} height={36} />
           </Button>
         </DropdownMenuTrigger>
         {/* NOTE: width를 조정할려면 Dropdown컴포넌트에서 min-w 수정 필요 */}
