5a9d1977007a5b7eeb47d6b22da5580e52a75ecf
JeonYumin94 <xosle94@gmail.com>
2024-07-30 12:17:25 +0900

    FE-27 :twisted_rightwards_arrows: 충돌오류 수정

--- src/hooks/userQueryHooks.ts
+++ src/hooks/userQueryHooks.ts
@@ -1,7 +1,7 @@
 import quries from '@/apis/queries';
 
 import { updateMe, createPresignedUrl } from '@/apis/user';
-import { GetUserRequestType, GetUserResponseType, PatchMeRequestType, PostPresignedUrlRequestType, PostPresignedUrlResponseType } from '@/schema/user';
+import { GetUserRequestType, PatchMeRequestType, PostPresignedUrlRequestType, PostPresignedUrlResponseType } from '@/schema/user';
 
 import { MutationOptions } from '@/types/query';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
@@ -10,7 +10,7 @@ export const useMeQuery = () => useQuery(quries.user.getMe());
 
 export const useUserQuery = (requset: GetUserRequestType) => useQuery(quries.user.getUser(requset));
 
-export const useUpdateMe = (options: MutationOptions<GetUserResponseType>) => {
+export const useUpdateMe = (options: MutationOptions<PatchMeRequestType>) => {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: (request: PatchMeRequestType) => updateMe(request),
