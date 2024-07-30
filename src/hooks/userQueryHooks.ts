1dd40d2d2d328b0ca5bb889e24030bdac0994dd6
JeonYumin94 <xosle94@gmail.com>
2024-07-30 12:11:04 +0900

    FE-27 :hammer: 충돌내역 수정

--- src/hooks/userQueryHooks.ts
+++ src/hooks/userQueryHooks.ts
@@ -1,7 +1,7 @@
 import quries from '@/apis/queries';
 
 import { updateMe, createPresignedUrl } from '@/apis/user';
-import { GetUserRequestType, PatchMeRequestType, PostPresignedUrlRequestType, PostPresignedUrlResponseType } from '@/schema/user';
+import { GetUserRequestType, GetUserResponseType, PatchMeRequestType, PostPresignedUrlRequestType, PostPresignedUrlResponseType } from '@/schema/user';
 
 import { MutationOptions } from '@/types/query';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
