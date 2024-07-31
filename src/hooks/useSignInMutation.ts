d919f4639ce8388ec64ec5b5317ad77a123f5949
MOON <muneon96@naver.com>
2024-07-31 20:14:23 +0900

    :wrench: 로그인 훅 에러처리 로직 수정

--- src/hooks/useSignInMutation.ts
+++ src/hooks/useSignInMutation.ts
@@ -2,6 +2,7 @@ import { postSignin } from '@/apis/auth';
 import { toast } from '@/components/ui/use-toast';
 import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'next/router';
+import { isAxiosError } from 'axios';
 
 const useSigninMutation = () => {
   const router = useRouter();
@@ -13,8 +14,34 @@ const useSigninMutation = () => {
       localStorage.setItem('refreshToken', data.refreshToken);
       router.push('/epigrams');
     },
-    onError: () => {
-      toast({ description: '이메일 혹은 비밀번호를 확인해주세요.', className: 'border-state-error text-state-error font-semibold' });
+    onError: (error) => {
+      if (!isAxiosError(error)) {
+        return;
+      }
+
+      const { status } = error.response || {};
+
+      if (status === 500) {
+        toast({
+          description: '서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요.',
+          className: 'border-state-error text-state-error font-semibold',
+        });
+        return;
+      }
+
+      // NOTE: status값은 항상 있으며 undefined와 숫자를 비교연산 할 수 없어 Number로 설정
+      if (Number(status) >= 500) {
+        toast({
+          description: '서버에서 예상치 못한 문제가 발생했습니다. 잠시 후 다시 시도해주세요.',
+          className: 'border-state-error text-state-error font-semibold',
+        });
+        return;
+      }
+
+      toast({
+        description: '이메일 혹은 비밀번호를 확인해주세요.',
+        className: 'border-state-error text-state-error font-semibold',
+      });
     },
   });
 };
