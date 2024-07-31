e3a8a8cf646a2c719c151a35e7f334bb8c2b182f
JeonYumin <40783675+JeonYumin94@users.noreply.github.com>
2024-07-31 17:21:27 +0900

    FE-27 :sparkles: 마이페이지 내 댓글목록 조회 (#137)
    * FE-39 :sparkles: 내 댓글목록 조회 API 연동
    
    * FE-39 :recycle: 내 에피그램 / 댓글조회 코드 리팩토링
    
    * FE-39 :lipstick: 내 에피그램 / 내 댓글 탭 선택 UI
    
    * FE-39 :sparkles: 내 에피그램/댓글 카운트 훅 추가
    
    * FE-39 :sparkles: 내 댓글목록 조회 기능 구현

--- src/pageLayout/MypageLayout/MyContent.tsx
+++ src/pageLayout/MypageLayout/MyContent.tsx
@@ -4,51 +4,111 @@ import MyEpigrams from '@/user/ui-content/MyEpigrams';
 import Image from 'next/image';
 import { useToast } from '@/components/ui/use-toast';
 import { EpigramsResponse } from '@/types/epigram.types';
+import { CommentResponseType } from '@/schema/comment';
+import useCommentsHook from '@/hooks/useCommentsHook';
+import useGetMyContentHook from '@/hooks/useGetMyContentHook';
+import { GetMyContentCountType } from '@/schema/user';
+import MyComment from '@/user/ui-content/MyComment';
+import UserInfo from '@/types/user';
 import spinner from '../../../public/spinner.svg';
 
 interface MyContentProps {
-  userId: number;
+  user: UserInfo;
 }
 
-export default function MyContent({ userId }: MyContentProps) {
+export default function MyContent({ user }: MyContentProps) {
   const limit = 3;
-  const [cursor, setCursor] = useState(0);
-  const [epigrams, setEpigrams] = useState<EpigramsResponse>({ totalCount: 0, nextCursor: null, list: [] });
   const [isLoadingMore, setIsLoadingMore] = useState(false);
+  const [selectedTab, setSelectedTab] = useState<'epigrams' | 'comments'>('epigrams');
   const { toast } = useToast();
 
+  /** ************ 내 에피그램/댓글 카운트 조회 ************* */
+  const [totalCount, setTotalCount] = useState<GetMyContentCountType>({ epigramCount: 0, commentCount: 0 });
+  const { data: count } = useGetMyContentHook({ id: user.id });
+  useEffect(() => {
+    if (count) {
+      setTotalCount({ epigramCount: count.epigramCount, commentCount: count.commentCount });
+    }
+  }, [count]);
+
+  /** ************ 내 에피그램 조회 ************* */
+  const [epigramCursor, setEpigramCursor] = useState<number>(0);
+  const [epigrams, setEpigrams] = useState<EpigramsResponse>({ totalCount: 0, nextCursor: null, list: [] });
   const epigramsRequest = {
     limit,
-    cursor,
-    writerId: userId,
+    cursor: epigramCursor,
+    writerId: user.id,
   };
-  const { data, isLoading, error } = useGetEpigrams(epigramsRequest);
+  const { data: epigramsData, isLoading: isEpigramsLoading, error: epigramsError } = useGetEpigrams(epigramsRequest);
 
+  /** ************ 내 댓글 조회 ************* */
+  const [commentCursor, setCommentCursor] = useState<number>(0);
+  const [comments, setComments] = useState<CommentResponseType>({ totalCount: 0, nextCursor: null, list: [] });
+  const commentsRequest = {
+    limit,
+    cursor: commentCursor,
+    id: user.id,
+  };
+  const { data: commentData, isLoading: isCommentsLoading, error: commentsError } = useCommentsHook(commentsRequest);
+
+  // [내 에피그램] 탭 선택 시
   useEffect(() => {
-    if (data && data.list.length > 0) {
+    if (selectedTab === 'epigrams' && epigramsData) {
       setEpigrams((prev) => ({
-        totalCount: data.totalCount,
-        nextCursor: data.nextCursor,
-        list: [...prev.list, ...data.list],
+        totalCount: epigramsData.totalCount,
+        nextCursor: epigramsData.nextCursor,
+        list: [...prev.list, ...epigramsData.list],
       }));
       setIsLoadingMore(false);
     }
-  }, [data]);
+  }, [epigramsData, selectedTab]);
 
-  const handleMoreEpigramLoad = () => {
-    if (epigrams.nextCursor !== null) {
-      setCursor(epigrams.nextCursor);
+  // [내 댓글] 탭 선택 시
+  useEffect(() => {
+    if (selectedTab === 'comments' && commentData) {
+      setComments((prev) => ({
+        totalCount: commentData.totalCount,
+        nextCursor: commentData.nextCursor,
+        list: [...prev.list, ...commentData.list],
+      }));
+      setIsLoadingMore(false);
+    }
+  }, [commentData, selectedTab]);
+
+  // 더보기 버튼 클릭 시
+  const handleMoreLoad = () => {
+    if (selectedTab === 'epigrams' && epigrams.nextCursor) {
+      setEpigramCursor(epigrams.nextCursor);
+      setIsLoadingMore(true);
+    } else if (selectedTab === 'comments' && comments.nextCursor) {
+      setCommentCursor(comments.nextCursor);
       setIsLoadingMore(true);
     }
   };
 
-  if (isLoading && !isLoadingMore) {
+  // [내 에피그램] [내 댓글] 탭 선택
+  const handleTabClick = (tab: 'epigrams' | 'comments') => {
+    setSelectedTab(tab);
+    // 데이터 초기화
+    if (tab === 'epigrams') {
+      setEpigrams({ totalCount: 0, nextCursor: null, list: [] });
+      setEpigramCursor(0);
+    } else {
+      setComments({ totalCount: 0, nextCursor: null, list: [] });
+      setCommentCursor(0);
+    }
+    setIsLoadingMore(false);
+  };
+
+  // 로딩 중
+  if ((isEpigramsLoading || isCommentsLoading) && !isLoadingMore) {
     return <Image src={spinner} alt='로딩중' width={200} height={200} />;
   }
 
-  if (error) {
+  // 에러
+  if (epigramsError || commentsError) {
     toast({
-      description: error.message,
+      description: epigramsError?.message || commentsError?.message,
       className: 'border-state-error text-state-error font-semibold',
     });
   }
@@ -56,16 +116,27 @@ export default function MyContent({ userId }: MyContentProps) {
   return (
     <div className='flex flex-col w-full lg:max-w-[640px] md:max-w-[640px] gap-12'>
       <div className='inline-flex gap-6'>
-        <button type='button' className='text-black-600 font-semibold text-2xl'>
-          내 에피그램({epigrams.totalCount})
+        <button
+          type='button'
+          className={`font-semibold text-2xl ${selectedTab === 'epigrams' ? 'cursor-default text-black-600 ' : 'cursor-pointer text-neutral-400 '}`}
+          onClick={() => selectedTab !== 'epigrams' && handleTabClick('epigrams')}
+          disabled={selectedTab === 'epigrams'}
+        >
+          내 에피그램({totalCount.epigramCount})
         </button>
-        <button type='button' className='text-neutral-400 font-semibold text-2xl'>
-          내 댓글(0)
+        <button
+          type='button'
+          className={`font-semibold text-2xl ${selectedTab === 'comments' ? 'cursor-default text-black-600 ' : 'cursor-pointer text-neutral-400 '}`}
+          onClick={() => selectedTab !== 'comments' && handleTabClick('comments')}
+          disabled={selectedTab === 'comments'}
+        >
+          내 댓글({totalCount.commentCount})
         </button>
       </div>
       <div className='w-full py-[36px]'>
         <div className='flex flex-col gap-[48px]'>
-          <MyEpigrams epigrams={epigrams.list} totalCount={epigrams.totalCount} onMoreEpigramLoad={handleMoreEpigramLoad} />
+          {selectedTab === 'epigrams' && <MyEpigrams epigrams={epigrams.list} totalCount={epigrams.totalCount} onMoreEpigramLoad={handleMoreLoad} />}
+          {selectedTab === 'comments' && <MyComment comments={comments.list} totalCount={comments.totalCount} onMoreEpigramLoad={handleMoreLoad} />}
           {isLoadingMore && (
             <div className='w-full flex items-center justify-center lg:mt-[70px] md:mt-[50px]'>
               <Image src={spinner} alt='로딩중' width={200} height={200} />
---
+++ src/user/ui-content/MyComment.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { sizeStyles, textSizeStyles, gapStyles, paddingStyles, contentWidthStyles } from '@/styles/CommentCardStyles';
+import { CommentType } from '@/schema/comment';
+import { Button } from '@/components/ui/button';
+import NONE_EPI from '../../../public/none-epi.svg';
+
+interface MyCommentProps {
+  comments: CommentType[];
+  totalCount: number;
+  onMoreEpigramLoad: () => void;
+}
+
+function MyComment({ comments, totalCount, onMoreEpigramLoad }: MyCommentProps) {
+  const router = useRouter();
+
+  const handleMoveToMain = () => {
+    router.push('/epigrams');
+  };
+
+  return totalCount > 0 ? (
+    <div className='flex flex-col'>
+      {comments.map((comment) => {
+        const formattedDate = new Date(comment.createdAt).toLocaleString(); // createdAt을 comment에서 가져옴
+
+        return (
+          <div
+            key={comment.id}
+            className={`bg-background-100 border-t border-slate-300 flex-col justify-start items-start gap-2.5 inline-flex ${sizeStyles.sm} ${sizeStyles.md} ${sizeStyles.lg} ${paddingStyles.sm} ${paddingStyles.md} ${paddingStyles.lg}`}
+          >
+            <div className='justify-start items-start gap-4 inline-flex'>
+              <div className='w-12 h-12 relative'>
+                <div className='w-12 h-12 bg-zinc-300 rounded-full overflow-hidden flex items-center justify-center relative'>
+                  <Image src={comment.writer.image || '/ProfileTestImage.jpg'} alt='프로필 이미지' width={50} height={50} className='rounded-full' />
+                </div>
+              </div>
+              <div
+                className={`flex-col justify-start items-start ${gapStyles.sm} ${gapStyles.md} ${gapStyles.lg} inline-flex ${contentWidthStyles.sm} ${contentWidthStyles.md} ${contentWidthStyles.lg}`}
+              >
+                <div className='justify-between items-center w-full inline-flex'>
+                  <div className='justify-start items-start gap-2 flex'>
+                    <div className={`text-zinc-600 font-normal font-pretendard leading-normal ${textSizeStyles.sm.name} ${textSizeStyles.md.name} ${textSizeStyles.lg.name}`}>
+                      {comment.writer.nickname}
+                    </div>
+                    <div className={`text-zinc-600 font-normal font-pretendard leading-normal ${textSizeStyles.sm.time} ${textSizeStyles.md.time} ${textSizeStyles.lg.time}`}>
+                      {formattedDate} {/* 중괄호 추가 */}
+                    </div>
+                  </div>
+                  <div className='justify-start items-start gap-4 flex'>
+                    <div className={`text-neutral-700 underline leading-[18px] cursor-pointer ${textSizeStyles.sm.action} ${textSizeStyles.md.action} ${textSizeStyles.lg.action}`}>수정</div>
+                    <div className={`text-red-400 underline leading-[18px] cursor-pointer ${textSizeStyles.sm.action} ${textSizeStyles.md.action} ${textSizeStyles.lg.action}`}>삭제</div>
+                  </div>
+                </div>
+                <div
+                  className={`w-full text-zinc-800 font-normal font-pretendard ${textSizeStyles.sm.content} ${textSizeStyles.md.content} ${textSizeStyles.lg.content} ${contentWidthStyles.sm} ${contentWidthStyles.md} ${contentWidthStyles.lg}`}
+                >
+                  {comment.content}
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+      {totalCount > comments.length && (
+        <div className='w-full flex items-center justify-center'>
+          <Button className='text-slate-400 border border-slate-300 rounded-[100px] py-[12px] px-[20px]' onClick={onMoreEpigramLoad}>
+            + 더보기
+          </Button>
+        </div>
+      )}
+    </div>
+  ) : (
+    <div className='flex flex-col gap-4 justify-center items-center'>
+      <Image src={NONE_EPI} alt='돋보기아이콘' width={144} height={144} />
+      <div className='flex flex-col gap-[48px] justify-center items-center'>
+        <div className='text-center'>
+          <p>아직 작성한 댓글이 없어요!</p>
+          <p>댓글을 달고 다른 사람들과 교류해보세요.</p>
+        </div>
+        <Button className='px-[18px] py-3 rounded-[100px] border border-neutral-200 justify-center items-center gap-1' onClick={handleMoveToMain}>
+          에피그램 둘러보기
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default MyComment;
