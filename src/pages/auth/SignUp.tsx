b451058644c659ffc32af35775a0b94c55e4e85f
MOON <muneon96@naver.com>
2024-07-31 23:06:51 +0900

    :sparkles: 회원가입 폼 공백 제거 추가

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -22,6 +22,10 @@ export default function SignUp() {
     },
   });
 
+  const trimWhitespace = (fieldName: keyof PostSignUpRequestType, value: string) => {
+    form.setValue(fieldName, value.trim(), { shouldValidate: true, shouldDirty: true });
+  };
+
   return (
     <div className='flex flex-col justify-center items-center bg-background-100 w-full min-h-screen'>
       <header className='h-full mb-[50px] md:mb-[60px]'>
@@ -44,6 +48,7 @@ export default function SignUp() {
                       placeholder='이메일'
                       className={`lg:h-16 h-11 px-4 lg:text-xl md:text-base placeholder-blue-400 rounded-xl bg-blue-200 font-pretendard ${fieldState.invalid ? 'border-2 border-state-error' : ''}`}
                       {...field}
+                      onBlur={(e) => trimWhitespace('email', e.target.value)}
                     />
                   </FormControl>
                   <FormMessage className='flex justify-end text-[13px] text-state-error' />
@@ -62,6 +67,7 @@ export default function SignUp() {
                       placeholder='비밀번호'
                       className={`lg:h-16 h-11 px-4 lg:text-xl md:text-base placeholder-blue-400 rounded-xl bg-blue-200 font-pretendard ${fieldState.invalid ? 'border-2 border-state-error' : ''}`}
                       {...field}
+                      onBlur={(e) => trimWhitespace('password', e.target.value)}
                     />
                   </FormControl>
                   <FormMessage className='flex justify-end text-[13px] text-state-error' />
@@ -79,6 +85,7 @@ export default function SignUp() {
                       placeholder='비밀번호 확인'
                       className={`lg:h-16 h-11 px-4 lg:text-xl md:text-base placeholder-blue-400 rounded-xl bg-blue-200 font-pretendard ${fieldState.invalid ? 'border-2 border-state-error' : ''}`}
                       {...field}
+                      onBlur={(e) => trimWhitespace('passwordConfirmation', e.target.value)}
                     />
                   </FormControl>
                   <FormMessage className='flex justify-end text-[13px] text-state-error' />
@@ -97,6 +104,7 @@ export default function SignUp() {
                       placeholder='닉네임'
                       className={`lg:h-16 h-11 px-4 lg:text-xl md:text-base placeholder-blue-400 rounded-xl bg-blue-200 font-pretendard ${fieldState.invalid ? 'border-2 border-state-error' : ''}`}
                       {...field}
+                      onBlur={(e) => trimWhitespace('nickname', e.target.value)}
                     />
                   </FormControl>
                   <FormMessage className='flex justify-end text-[13px] text-state-error' />
