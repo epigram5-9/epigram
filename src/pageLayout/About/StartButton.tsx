5cfdc12951371d4f8e511b181ce3b07af1e2a4f4
NEWJIN <109906670+newjinlee@users.noreply.github.com>
2024-08-01 21:42:21 +0900

    FE-21 :lipstick: 반응형 디자인 구현 (#150)
    * FE-21 :lipstick: 반응형 디자인
    
    * FE-21 :lipstick: 로고 사이즈 수정

--- src/pageLayout/About/StartButton.tsx
+++ src/pageLayout/About/StartButton.tsx
@@ -24,8 +24,18 @@ function StartButton({ className = '', children }: StartButtonProps) {
   };
 
   return (
-    <button type='button' className={`w-[286px] h-16 px-4 bg-zinc-700 rounded-xl justify-center items-center gap-2 inline-flex ${className}`} onClick={handleClick}>
-      <div className='text-white text-xl font-semibold font-pretendard leading-loose'>{children}</div>
+    <button
+      type='button'
+      className={`bg-zinc-700 justify-center items-center gap-2 inline-flex 
+                  w-[112px] h-[48px] px-4 
+                  md:w-[112px] md:h-[48px] md:px-4 
+                  lg:w-[286px] lg:h-[64px] lg:px-4 
+                  rounded-[12px]
+                  opacity-100 
+                  ${className}`}
+      onClick={handleClick}
+    >
+      <div className='text-white text-[16px] lg:text-[20px] font-semibold font-pretendard leading-loose'>{children}</div>
     </button>
   );
 }
