4a3738586cdefc12d698c0ed6b2b9dd507455b35
Woojiseok <jisurk154@naver.com>
2024-08-01 16:22:00 +0900

    FE-54🐛 build error 해결

--- src/pages/epigrams/[id]/index.tsx
+++ src/pages/epigrams/[id]/index.tsx
@@ -22,7 +22,7 @@ function DetailPage() {
 
   return (
     <div className='flex flex-col min-h-screen '>
-      <Header icon='search' routerPage='/search' isLogo insteadOfLogo='' isProfileIcon isShareIcon={false} isButton={false} textInButton='' disabled={false} onClick={() => {}} />
+      <Header icon='search' isLogo insteadOfLogo='' isProfileIcon isShareIcon={false} isButton={false} textInButton='' disabled={false} onClick={() => {}} />
       <EpigramFigure epigram={epigram} currentUserId={userData?.id} />
       <EpigramComment epigramId={epigram.id} currentUserId={userData?.id} userImage={userData?.image} />
     </div>
