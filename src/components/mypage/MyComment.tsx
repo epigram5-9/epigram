d2c7d54ffbaea8fdf351cbfddce19eca0d0264af
JeonYumin94 <xosle94@gmail.com>
2024-08-06 12:55:27 +0900

    FE-86 :recycle: 비공개 댓글 자물쇠 이미지 추가

--- src/components/mypage/MyComment.tsx
+++ src/components/mypage/MyComment.tsx
@@ -6,8 +6,8 @@ import { CommentType } from '@/schema/comment';
 import { Button } from '@/components/ui/button';
 import DeleteAlertModal from '@/components/epigram/DeleteAlertModal';
 import CommentTextarea from '@/components/epigram/Comment/CommentTextarea';
-import NONE_EPI from '../../../public/none-epi.svg';
 import getCustomRelativeTime from '@/lib/dateUtils';
+import NONE_EPI from '../../../public/none-epi.svg';
 
 const sizeStyles = {
   sm: 'w-[360px]',
@@ -64,69 +64,68 @@ function MyComment({ comments, totalCount, onMoreEpigramLoad, onDeleteComment, o
 
   return totalCount > 0 ? (
     <div className='flex flex-col'>
-      {comments.map((comment) => {
-        return (
-          <div
-            key={comment.id}
-            className={`bg-background-100 border-t border-slate-300 flex-col justify-start items-start gap-2.5 inline-flex ${sizeStyles.sm} ${sizeStyles.md} ${sizeStyles.lg} ${paddingStyles.sm} ${paddingStyles.md} ${paddingStyles.lg}`}
-          >
-            <div className='justify-start items-start gap-4 inline-flex w-full'>
-              <div className='w-12 h-12 relative'>
-                <div className='w-12 h-12 bg-zinc-300 rounded-full overflow-hidden flex items-center justify-center relative'>
-                  <Image src={comment.writer.image || '/ProfileTestImage.jpg'} alt='프로필 이미지' width={50} height={50} className='rounded-full' />
+      {comments.map((comment) => (
+        <div
+          key={comment.id}
+          className={`bg-background-100 border-t border-slate-300 flex-col justify-start items-start gap-2.5 inline-flex ${sizeStyles.sm} ${sizeStyles.md} ${sizeStyles.lg} ${paddingStyles.sm} ${paddingStyles.md} ${paddingStyles.lg}`}
+        >
+          <div className='justify-start items-start gap-4 inline-flex w-full'>
+            <div className='w-12 h-12 relative'>
+              <div className='w-12 h-12 bg-zinc-300 rounded-full overflow-hidden flex items-center justify-center relative'>
+                <Image src={comment.writer.image || '/ProfileTestImage.jpg'} alt='프로필 이미지' width={50} height={50} className='rounded-full' />
+              </div>
+            </div>
+            {editingCommentId === comment.id ? (
+              <div className='w-full mb-4'>
+                <CommentTextarea epigramId={comment.epigramId} editingComment={comment} onEditComplete={handleEditComplete} />
+              </div>
+            ) : (
+              <div
+                className={`flex-col justify-start items-start ${gapStyles.sm} ${gapStyles.md} ${gapStyles.lg} inline-flex ${contentWidthStyles.sm} ${contentWidthStyles.md} ${contentWidthStyles.lg}`}
+              >
+                <div className='justify-between items-center w-full inline-flex'>
+                  <div className='justify-start items-center gap-2 flex'>
+                    <div className={`text-zinc-600 font-normal font-pretendard leading-normal ${textSizeStyles.sm.name} ${textSizeStyles.md.name} ${textSizeStyles.lg.name}`}>
+                      {comment.writer.nickname}
+                    </div>
+                    <div className={`text-zinc-600 font-normal font-pretendard leading-normal ${textSizeStyles.sm.time} ${textSizeStyles.md.time} ${textSizeStyles.lg.time}`}>
+                      {getCustomRelativeTime(comment.createdAt)}
+                    </div>
+                    {comment.isPrivate && <Image src='/icon/privateIcon.png' width={16} height={16} alt='비공개' />}
+                  </div>
+                  <div className='justify-start items-start gap-4 flex'>
+                    <Button
+                      onClick={() => handleEditComment(comment)}
+                      className={`text-neutral-700 underline leading-[18px] cursor-pointer p-0 ${textSizeStyles.sm.action} ${textSizeStyles.md.action} ${textSizeStyles.lg.action}`}
+                    >
+                      수정
+                    </Button>
+                    <Button
+                      onClick={() => {
+                        setSelectedCommentId(comment.id);
+                        setIsDeleteModalOpen(true);
+                      }}
+                      type='button'
+                      className={`text-red-400 underline leading-[18px] cursor-pointer p-0 ${textSizeStyles.sm.action} ${textSizeStyles.md.action} ${textSizeStyles.lg.action}`}
+                    >
+                      삭제
+                    </Button>
+                  </div>
+                </div>
+                <div
+                  onClick={() => handleCommentClick(comment.epigramId)}
+                  onKeyDown={(event) => handleKeyDown(event, comment.epigramId)}
+                  role='button'
+                  tabIndex={0}
+                  className={`w-full text-zinc-800 font-normal font-pretendard ${textSizeStyles.sm.content} ${textSizeStyles.md.content} ${textSizeStyles.lg.content} ${contentWidthStyles.sm} ${contentWidthStyles.md} ${contentWidthStyles.lg}`}
+                >
+                  {comment.content}
                 </div>
               </div>
-              {editingCommentId === comment.id ? (
-                <div className='w-full mb-4'>
-                  <CommentTextarea epigramId={comment.epigramId} editingComment={comment} onEditComplete={handleEditComplete} />
-                </div>
-              ) : (
-                <div
-                  className={`flex-col justify-start items-start ${gapStyles.sm} ${gapStyles.md} ${gapStyles.lg} inline-flex ${contentWidthStyles.sm} ${contentWidthStyles.md} ${contentWidthStyles.lg}`}
-                >
-                  <div className='justify-between items-center w-full inline-flex'>
-                    <div className='justify-start items-start gap-2 flex'>
-                      <div className={`text-zinc-600 font-normal font-pretendard leading-normal ${textSizeStyles.sm.name} ${textSizeStyles.md.name} ${textSizeStyles.lg.name}`}>
-                        {comment.writer.nickname}
-                      </div>
-                      <div className={`text-zinc-600 font-normal font-pretendard leading-normal ${textSizeStyles.sm.time} ${textSizeStyles.md.time} ${textSizeStyles.lg.time}`}>
-                        {getCustomRelativeTime(comment.createdAt)}
-                      </div>
-                    </div>
-                    <div className='justify-start items-start gap-4 flex'>
-                      <Button
-                        onClick={() => handleEditComment(comment)}
-                        className={`text-neutral-700 underline leading-[18px] cursor-pointer p-0 ${textSizeStyles.sm.action} ${textSizeStyles.md.action} ${textSizeStyles.lg.action}`}
-                      >
-                        수정
-                      </Button>
-                      <Button
-                        onClick={() => {
-                          setSelectedCommentId(comment.id);
-                          setIsDeleteModalOpen(true);
-                        }}
-                        type='button'
-                        className={`text-red-400 underline leading-[18px] cursor-pointer p-0 ${textSizeStyles.sm.action} ${textSizeStyles.md.action} ${textSizeStyles.lg.action}`}
-                      >
-                        삭제
-                      </Button>
-                    </div>
-                  </div>
-                  <div
-                    onClick={() => handleCommentClick(comment.epigramId)}
-                    onKeyDown={(event) => handleKeyDown(event, comment.epigramId)}
-                    role='button'
-                    tabIndex={0}
-                    className={`w-full text-zinc-800 font-normal font-pretendard ${textSizeStyles.sm.content} ${textSizeStyles.md.content} ${textSizeStyles.lg.content} ${contentWidthStyles.sm} ${contentWidthStyles.md} ${contentWidthStyles.lg}`}
-                  >
-                    {comment.content}
-                  </div>
-                </div>
-              )}
-            </div>
+            )}
           </div>
-        );
-      })}
+        </div>
+      ))}
       {totalCount > comments.length && (
         <div className='w-full flex items-center justify-center'>
           <Button className='text-slate-400 border border-slate-300 rounded-[100px] py-[12px] px-[20px]' onClick={onMoreEpigramLoad}>
