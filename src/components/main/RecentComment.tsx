ac2bf6b42bbc2c45e52c8ebd681729a454758e4b
NEWJIN <winniepooh00@naver.com>
2024-08-07 15:01:45 +0900

    FE-13 :fire: 스피너 아이콘 배경색 삭제

--- src/components/main/RecentComment.tsx
+++ src/components/main/RecentComment.tsx
@@ -43,7 +43,7 @@ function RecentComments() {
           <CommentCard key={comment.id} writer={comment.writer} content={comment.content} createdAt={new Date(comment.createdAt)} status='view' />
         ))}
         {isLoadingMore && (
-          <div className='w-full flex items-center justify-center lg:mt-[70px] md:mt-[50px]'>
+          <div className='w-full flex items-center justify-center bg-transparent lg:mt-[70px] md:mt-[50px]'>
             <Image src={spinner} alt='로딩중' width={50} height={50} />
           </div>
         )}
